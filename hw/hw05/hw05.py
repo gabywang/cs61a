02eaa3c5d469ee3b6a20bfe4dcef6095243e5b3d
Xiaowen Wang <gabywangxiaowen@berkeley.edu>
2017-09-24 15:01:50 -0700

    Complete hw05, except the extra question Q9. Will finish this part when free.

--- hw/hw05/hw05.py
+++ hw/hw05/hw05.py
@@ -149,10 +149,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -168,22 +170,28 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+    p1 = lower_bound(x) - lower_bound(y)
+    p2 = lower_bound(x) - upper_bound(y)
+    p3 = upper_bound(x) - lower_bound(y)
+    p4 = upper_bound(x) - upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert not (lower_bound(y) < 0 and upper_bound(y) > 0)
     reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -206,11 +214,18 @@ def check_par():
     True
     """
     r1 = interval(1, 1) # Replace this line!
-    r2 = interval(1, 1) # Replace this line!
-    return r1, r2
+    r2 = interval(1, 2) # Replace this line!
+    return par1(r1, r2), par2(r1,r2)
 
 def multiple_references_explanation():
-    return """The multiple reference problem..."""
+    return """The multiple reference problem is caused by the type of interval.
+    When we refer to the same inteval twice, we may assume two differtent true
+    values for the same inteval. Hence, the result of inteval may become larger
+    than it suppose to be.
+
+    This is also the reason why par2 is better than par1, since it never refers
+    to the same interval twice.
+    """
 
 def quadratic(x, a, b, c):
     """Return the interval that is the range of the quadratic defined by
@@ -222,6 +237,14 @@ def quadratic(x, a, b, c):
     '0 to 10'
     """
     "*** YOUR CODE HERE ***"
+    extreme = - b / (2 * a)
+    f = lambda t: a * t * t + b * t + c
+    if extreme > lower_bound(x) and extreme < upper_bound(x):
+        return interval(min(f(extreme), f(lower_bound(x)), f(upper_bound(x))), \
+        max(f(extreme), f(lower_bound(x)), f(upper_bound(x))))
+    else:
+        return interval(min(f(lower_bound(x)), f(upper_bound(x))), \
+        max(f(lower_bound(x)), f(upper_bound(x))))
 
 def polynomial(x, c):
     """Return the interval that is the range of the polynomial defined by
