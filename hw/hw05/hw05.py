5611c6bb3e7526e07929731018b1c9b8983e9672
Xiaowen Wang <gabywangxiaowen@berkeley.edu>
2017-09-25 15:57:14 -0700

    add the comments

--- hw/hw05/hw05.py
+++ hw/hw05/hw05.py
@@ -134,6 +134,17 @@ def move_stack(n, start, end):
     "*** YOUR CODE HERE ***"
     #lst = [[] for x in range(3)]
     #lst[start-1] = [x for x in range(n)]
+    """
+    To solve this problem, we simply need to consider two cases:
+        The simpliest case: n = 1.
+        Consider the relationship between n and (n-1).
+    It's obvious that for case n, the last step is to move from its current place \
+    to the end. Hence, case n-1 should be in the alternative place. \
+    The order should be:
+        n-1 move to the alternative place.
+        n move to the end. And we have print_move(start, end)
+        n-1 move from the alternative place to the end.
+    """
 
     rods = [1, 2, 3]
     if n > 0:
