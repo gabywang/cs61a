106b7262fa4e09a651238eddaf33ca3de902de95
Xiaowen Wang <gabywangxiaowen@berkeley.edu>
2017-09-26 16:56:37 -0700

    Complete Q3

--- lab/lab05/tests/dicts.py
+++ lab/lab05/tests/dicts.py
@@ -8,32 +8,25 @@ test = {
           'code': r"""
           >>> pokemon = {'pikachu': 25, 'dragonair': 148, 'mew': 151}
           >>> pokemon['pikachu']
-          cb65f2ff357d7368f6d293aaf0bbd356
-          # locked
+          25
           >>> len(pokemon)
-          74689fcda5421388b764b40ec8de8ccd
-          # locked
+          3
           >>> pokemon['jolteon'] = 135
           >>> pokemon['ditto'] = 25
           >>> len(pokemon)
-          36823867c25d95f8a792b4dde2bf0d63
-          # locked
+          5
           >>> sorted(list(pokemon.keys())) # Alphabetically sorted list of pokemon's keys
-          46a11a9ef598a571fc5e656e76cc6618
-          # locked
+          ['ditto', 'dragonair', 'jolteon', 'mew', 'pikachu']
           >>> 'mewtwo' in pokemon
-          ac667055c8e3c84ad7260b0fefa2e007
-          # locked
+          False
           >>> pokemon['ditto'] = pokemon['jolteon']
           >>> sorted(list(pokemon.keys())) # Alphabetically sorted list of pokemon's keys
-          46a11a9ef598a571fc5e656e76cc6618
-          # locked
+          ['ditto', 'dragonair', 'jolteon', 'mew', 'pikachu']
           >>> pokemon['ditto']
-          c375daa87e984186b739dee6d6f04f1b
-          # locked
+          135
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         }
       ],
       'scored': False,
@@ -45,14 +38,12 @@ test = {
           'code': r"""
           >>> letters = {'a': 1, 'b': 2, 'c': 3}
           >>> 'a' in letters
-          4975a2633e94dd9ea1ce929c1df08a3b
-          # locked
+          True
           >>> 2 in letters
-          ac667055c8e3c84ad7260b0fefa2e007
-          # locked
+          False
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         }
       ],
       'scored': False,
@@ -65,28 +56,22 @@ test = {
           >>> food = {'bulgogi': 10, 'falafel': 4, 'ceviche': 7}
           >>> food['ultimate'] = food['bulgogi'] + food['ceviche']
           >>> food['ultimate']
-          6c4c5c2026b2b916aff21d836960bbd9
-          # locked
+          17
           >>> len(food)
-          fef77a143fa87e746554afe9ebb16a3d
-          # locked
+          4
           >>> food['ultimate'] += food['falafel']
           >>> food['ultimate']
-          286a63c09649ab2e465e9e1abab82eba
-          # locked
+          21
           >>> sorted(list(food.keys())) # sorted takes in a list and returns a new sorted list
-          5c80b6b1619d37b4afb811e76ebc2c56
-          # locked
+          ['bulgogi', 'ceviche', 'falafel', 'ultimate']
           >>> food['bulgogi'] = food['falafel']
           >>> len(food)
-          fef77a143fa87e746554afe9ebb16a3d
-          # locked
+          4
           >>> 'gogi' in food
-          ac667055c8e3c84ad7260b0fefa2e007
-          # locked
+          False
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         }
       ],
       'scored': False,
